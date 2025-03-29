933b62cdb0bbb5a71122f16d8d9c8347af2d82fd
Eric Mika <eric@ericmika.com>
2025-03-29 17:41:01 -0400

    Remove errant logging.

--- src/lib/utilities/path.ts
+++ src/lib/utilities/path.ts
@@ -113,6 +113,5 @@ export function pathObjectSetAll(source: PathObject, target: PathObject): void {
 export function pathIsFolderNote(filePath: string): boolean {
 	const bareFileName = path.basename(filePath, path.extname(filePath)).toLowerCase()
 	const parentFolderName = path.basename(path.dirname(filePath)).toLowerCase()
-	console.log(bareFileName, parentFolderName)
 	return bareFileName === parentFolderName
 }
