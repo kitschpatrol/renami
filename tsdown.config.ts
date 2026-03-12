c30eeac09b72318c0c180905f00868bfa0dca4e3
Eric Mika <eric@ericmika.com>
2026-03-12 15:23:26 -0400

    Update dependencies.

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'tsdown'
 
 export default defineConfig([
+	// CLI
 	{
 		dts: false,
 		entry: 'src/bin/cli.ts',
@@ -8,6 +9,7 @@ export default defineConfig([
 		outDir: 'dist/bin',
 		platform: 'node',
 	},
+	// Library
 	{
 		alias: {
 			// Polyfill for filenamify's use of node:path
